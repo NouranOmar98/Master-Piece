a0b11e59717f045b03aaf06c3483220f3f19a44e
NouranOmar98 <norankinane@gmail.com>
2023-03-05 16:46:09 +0300

    Project

---
+++ Controllers/ErrorPageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MasterPiece.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        // GET: ErrorPage
+        public ActionResult ErrorPage()
+        {
+            return View();
+        }
+        public ActionResult ErrorPage2()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/FAQController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MasterPiece.Controllers
+{
+    public class FAQController : Controller
+    {
+        // GET: FAQ
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+
+    }
+}
